5a47c1f6deb229ff4121aed2e103e32e13b69aa7
longislandicetea <haoluo3@illinois.edu>
2015-04-08 20:02:45 -0500

    calculate semantic similarity between phrases

---
+++ toy_experiments/phrase_semantic_sim.py
@@ -0,0 +1,13 @@
+from requests import get
+
+sss_url = "http://swoogle.umbc.edu/SimService/GetSimilarity"
+
+def sss(s1, s2, type='relation', corpus='webbase'):
+    try:
+        response = get(sss_url, params={'operation':'api','phrase1':s1,'phrase2':s2,'type':type,'corpus':corpus})
+        return float(response.text.strip())
+    except:
+        print 'Error in getting similarity for %s: %s' % ((s1,s2), response)
+        return 0.0
+
+print sss("data fusion", "data integration")
