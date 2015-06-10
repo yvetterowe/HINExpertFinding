ddd058a6a1d248ffeb8ad8465d40911ad4e8535f
longislandicetea <haoluo3@illinois.edu>
2015-06-10 13:35:50 -0400

    play with gensim word2vec

--- toy_experiments/word2vec_phrase_sim.py
+++ toy_experiments/word2vec_phrase_sim.py
@@ -3,13 +3,13 @@ from nltk.corpus import brown, treebank
 
 #w2v_model = Word2Vec(treebank.sents())
 #w2v_model.save('w2v_treebank')
-w2v_model = Word2Vec.load('w2v_treebank')
+w2v_model = Word2Vec.load('w2v_treebank') # w2v_model is a dictionary (key=word, value=feature vector)
 
 print w2v_model.most_similar('money', topn=5)
 
 #phrase_model = Phrases(treebank.sents())
 #phrase_model.save('phrase_treebank')
-phrase_model = Phrases.load('phrase_treebank')
+phrase_model = Phrases.load('phrase_treebank') # phrase_model is a dictionary (key=sentence with single tokens, value=sentence with phrases combined)
 sample_sent = [u'Pierre', u'Vinken', u',', u'61', u'years', u'old',
 			   u',', u'will', u'join', u'the', u'board', u'as', u'a', 
 			   u'nonexecutive', u'director', u'Nov.', u'29', u'.']
