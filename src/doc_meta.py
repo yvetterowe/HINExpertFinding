c837b8d15d5e74661b6871597b5c4869116060d2
longislandicetea <haoluo3@illinois.edu>
2015-06-17 17:25:57 -0400

    reformatting

--- src/doc_meta.py
+++ src/doc_meta.py
@@ -1,12 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Author: Hao Luo
 
-class DocMeta(object):
-	def __init__(self, doc_id, phrases, authors, venue):
-		self.doc_id = doc_id
-		self.phrases = phrases  # phrase count dict
-		self.authors = authors  # author id list
-		self.venue = venue  # venue id 
 
+class DocMeta(object):
+    def __init__(
+        self,
+        doc_id,
+        phrases,
+        authors,
+        venue,
+        ):
+        self.doc_id = doc_id
+        self.phrases = phrases  # phrase count dict
+        self.authors = authors  # author id list
+        self.venue = venue  # venue id
